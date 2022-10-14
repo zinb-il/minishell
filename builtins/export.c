53b98b5e0173fc1975db53fa66884b1997dd600c
zinb9419 <97052963+zinb9419@users.noreply.github.com>
2022-10-14 16:54:14 +0100

    fifth

--- builtins/export.c
+++ builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 18:49:47 by ibentour          #+#    #+#             */
-/*   Updated: 2022/10/14 00:26:54 by ziloughm         ###   ########.fr       */
+/*   Updated: 2022/10/14 16:53:40 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 int	check_chars(char *arg)
 {
-	int	ch;
+	int		ch;
 
 	ch = 0;
 	if (!ft_isalpha(arg[0]) && arg[0] != '_')
 	{
-		printf("export: %s': not a valid identifier\n", arg);
+		printf("export: `%s': not a valid identifier\n", arg);
 		return (1);
 	}
 	while (arg[ch] != '\0' && arg[ch] != '=')
 	{
-		if (!(ft_isalnum(arg[ch]) && arg[ch] != '_') && \
-		(arg[ch] == '+' && arg[ch + 1] != '='))
+		if ((!(ft_isalnum(arg[ch])) && arg[ch] != '_' && arg[ch] != '+') \
+			|| (arg[ch] == '+' && arg[ch + 1] != '='))
 		{
-			printf("export: %s': not a valid identifier *\n", arg);
+			printf("export: `%s': not a valid identifier\n", arg);
 			return (1);
 		}
 		ch++;
@@ -35,63 +35,60 @@ int	check_chars(char *arg)
 	return (0);
 }
 
-char	**find_first_ecl(char *arg, char ecl)
+static int	what_to_do(t_builtins *tl, char *att, char *arg)
 {
-	int		x;
-	char	**str;
+	if (ft_strcmp(att, tl->tmp->env_att) == 0)
+	{
+		if (arg[tl->i] && arg[tl->i] == '=' && arg[tl->i - 1] != '+')
+			tl->tmp->env_val = ft_strdup(arg + tl->i + 1);
+		else if (arg[tl->i] && arg[tl->i] == '=' && arg[tl->i - 1] == '+')
+		{
+			tl->val_p = tl->tmp->env_val;
+			if (!tl->tmp->env_val)
+				tl->tmp->env_val = ft_strdup(arg + tl->i + 1);
+			else
+				tl->tmp->env_val = ft_strjoin(tl->tmp->env_val, (arg + \
+														tl->i + 1));
+			free (tl->val_p);
+		}
+		return (1);
+	}
+	return (0);
+}
 
-	x = 0;
-	str = (char **) malloc (sizeof(char *) * 3);
-	while (arg[x] && arg[x] != ecl)
-		x++;
-	if (arg[x] && arg[x] == ecl)
+static	int	w_env_exist(char *att, char *arg, size_t i)
+{
+	t_builtins	tl;
+
+	tl.val_p = NULL;
+	tl.i = i;
+	tl.tmp = g_vars.env;
+	while (tl.tmp)
 	{
-		str[0] = ft_substr(arg, 0, x);
-		str[1] = ft_strdup(arg + x + 1);
-		str[2] = NULL;
+		if (what_to_do(&tl, att, arg))
+			return (1);
+		else
+			tl.tmp = tl.tmp->next;
 	}
-	else
-	{
-		str[0] = ft_strdup(arg);
-		str[1] = NULL;
-		str[2] = NULL;
-	}
-	return (str);
+	return (0);
 }
 
 int	env_exists(char *arg)
 {
-	t_env		*tmp;
-	char		*att;
 	size_t		i;
-	char		*val_p;
+	char		*att;
 
 	i = 0;
-	val_p = NULL;
-	tmp = g_vars.env;
 	while (arg[i] && arg[i] != '=')
 		i++;
 	if (arg[i - 1] && arg[i - 1] == '+')
 		att = ft_substr(arg, 0, i - 1);
 	else
 		att = ft_substr(arg, 0, i);
-	while (tmp)
+	if (w_env_exist(att, arg, i))
 	{
-		if (ft_strcmp(att, tmp->env_att) == 0)
-		{
-			if (arg[i] && arg[i] == '=' && arg[i - 1] != '+')
-				tmp->env_val = ft_strdup(arg + i + 1);
-			else if (arg[i] && arg[i] == '=' && arg[i - 1] == '+')
-			{
-				val_p = tmp->env_val;
-				tmp->env_val = ft_strjoin(tmp->env_val, (arg + i + 1));
-				free (val_p);
-			}
-			free (att);
-			return (1);
-		}
-		else
-			tmp = tmp->next;
+		free (att);
+		return (1);
 	}
 	free(att);
 	return (0);
--- builtins/export_util.c
+++ builtins/export_util.c
@@ -6,7 +6,7 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 00:07:09 by ziloughm          #+#    #+#             */
-/*   Updated: 2022/10/14 00:10:15 by ziloughm         ###   ########.fr       */
+/*   Updated: 2022/10/14 16:53:56 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,47 @@ void	export_values(char *arg)
 
 	if (check_chars(arg))
 		return ;
-	if (env_exists(arg) == 1)
+	if (env_exists(arg))
 		return ;
 	else
 	{
-		s = find_first_ecl(arg, '=');
+		s = find_first_ecl(arg);
 		if (!s)
 			return ;
-		ft_lstadd_back(&g_vars.env, ft_lstnew(ft_strdup(s[0]), \
-		ft_strdup(s[1])));
+		if (s[1])
+			ft_lstadd_back(&g_vars.env, ft_lstnew(ft_strdup(s[0]), \
+				ft_strdup(s[1])));
+		else
+			ft_lstadd_back(&g_vars.env, ft_lstnew(ft_strdup(s[0]), NULL));
 		free_dstr(s);
 	}
+}
+
+char	**find_first_ecl(char *arg)
+{
+	int		x;
+	char	**str;
+
+	x = 0;
+	str = (char **) malloc (sizeof(char *) * 3);
+	if (!str)
+		return (NULL);
+	while (arg[x] && (arg[x] != '+' && arg[x] != '='))
+		x++;
+	if (arg[x] && (arg[x] == '+' || arg[x] == '='))
+	{
+		str[0] = ft_substr(arg, 0, x);
+		if (arg[x] == '+')
+			str[1] = ft_strdup(arg + x + 2);
+		else
+			str[1] = ft_strdup(arg + x + 1);
+		str[2] = NULL;
+	}
+	else
+	{
+		str[0] = ft_strdup(arg);
+		str[1] = NULL;
+		str[2] = NULL;
+	}
+	return (str);
 }
